df296e5ede819858f2fa2be8fae32ad911233abe
sayedtaqui <sayedwp@gmail.com>
2025-07-17 17:46:34 +0530

    Add design config

---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  darkMode: 'class',
+  content: [
+    path.resolve(__dirname, './packages/design-system/src/**/*.{tsx,ts,js}'),
+    path.resolve(__dirname, './packages/design-system/src/**/*.{tsx,ts,js}'),
+  ],
+};
