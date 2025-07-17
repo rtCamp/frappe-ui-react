de4a81534efeafccb5faffa3ab6a08cd4d1f256a
sayedtaqui <sayedwp@gmail.com>
2025-07-17 19:58:51 +0530

    Setup tailwind css

--- packages/frappe-components/src/stories/Button.tsx
+++ packages/frappe-components/src/stories/Button.tsx
@@ -27,7 +27,7 @@ export const Button = ({
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      className={['text-3xl storybook-button', `storybook-button--${size}`, mode].join(' ')}
       style={{ backgroundColor }}
       {...props}
     >
