6e9fe470903f06d975d7e5083bd5f1b1a39e03c7
sayedtaqui <sayedwp@gmail.com>
2025-10-23 22:04:20 +0400

    Update title style

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -5,7 +5,7 @@ addons.setConfig({
   theme: create({
     base: "light",
     brandTitle:
-      '<span id="fur-title">frappe-ui-react</span> <span id="fur-version">(v1.0.0-beta.0)</span>', // update version as per package.json
+      '<h1 id="fur-title">frappe-ui-react</h1> <span id="fur-version">(v1.0.0-beta.0)</span>', // update version as per package.json
     brandUrl: undefined, // disables link on the title
     brandImage: undefined,
   }),
