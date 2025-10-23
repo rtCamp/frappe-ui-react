7bfc8404d03ed1f23bde5ae27d4a9ca5c12cb7d5
sayedtaqui <sayedwp@gmail.com>
2025-10-23 23:08:26 +0400

    Change sidebar selected background color

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -8,5 +8,13 @@ addons.setConfig({
       '<h1 id="fur-title">frappe-ui-react</h1> <span id="fur-version">(v1.0.0-beta.0)</span>', // update version as per package.json
     brandUrl: undefined, // disables link on the title
     brandImage: undefined,
+
+      // Sidebar/Toolbar active state color
+    barSelectedColor: 'rgb(153, 153, 153)', 
+                                          
+    // Primary accent color for buttons, links, focus states
+    colorPrimary: 'rgb(153, 153, 153)',   
+    colorSecondary: 'rgb(153, 153, 153)',
+    fontBase: '"Inter", sans-serif',
   }),
 });
