4d6a59201ee5b3f2a4a999f7708ba0347b04b8ad
Aditya Dhade <aditya.dhade@rtcamp.com>
2025-12-04 16:19:13 +0530

    Add stories and icon support for Switch

--- packages/frappe-ui-react/src/components/switch/switch.stories.tsx
+++ packages/frappe-ui-react/src/components/switch/switch.stories.tsx
@@ -9,24 +9,6 @@ const meta: Meta<typeof Switch> = {
   tags: ["autodocs"],
   component: Switch,
   parameters: { docs: { source: { type: "dynamic" } }, layout: "centered" },
-};
-export default meta;
-
-type StoryType = StoryObj<typeof Switch>;
-
-export const Label: StoryType = {
-  args: {
-    size: "sm",
-    label: "Enable Notifications",
-    description: "",
-    disabled: false,
-    value: false,
-    className: "",
-  },
-  render: (args) => {
-    const [checked, setChecked] = useState(args.value ?? false);
-    return <Switch {...args} value={checked} onChange={setChecked} />;
-  },
   argTypes: {
     size: {
       control: "select",
@@ -57,21 +39,128 @@ export const Label: StoryType = {
       control: "text",
       description: "CSS classes for the Switch container",
     },
+    labelClassName: {
+      control: "text",
+      description: "CSS classes for the Switch label",
+    },
+  },
+};
+export default meta;
+
+type Story = StoryObj<typeof Switch>;
+
+export const Plain: Story = {
+  args: {
+    size: "sm",
+    disabled: false,
+    value: false,
+  },
+  render: (args) => {
+    const [checked, setChecked] = useState(args.value ?? false);
+    return <Switch {...args} value={checked} onChange={setChecked} />;
   },
 };
 
-export const LabelAndDescription: StoryType = {
+export const Label: Story = {
+  args: {
+    size: "sm",
+    label: "Enable Notifications",
+    disabled: false,
+    value: false,
+  },
+  render: (args) => {
+    const [checked, setChecked] = useState(args.value ?? false);
+    return (
+      <div className="w-[266px]">
+        <Switch {...args} value={checked} onChange={setChecked} />
+      </div>
+    );
+  },
+};
+
+export const DescriptionAndIcon: Story = {
   args: {
     size: "sm",
     label: "Enable Notifications",
     description: "Get notified when something happens.",
     disabled: false,
     value: false,
-    className: "",
+  },
+  render: (args) => {
+    const [checked1, setChecked1] = useState(args.value ?? false);
+    const [checked2, setChecked2] = useState(args.value ?? false);
+    return (
+      <div className="flex flex-col gap-2 w-[266px]">
+        <Switch {...args} value={checked1} onChange={setChecked1} />
+        <Switch
+          {...args}
+          icon="inbox"
+          value={checked2}
+          onChange={setChecked2}
+          description="This has an icon."
+        />
+      </div>
+    );
+  },
+};
+
+export const Sizes: Story = {
+  args: {
+    label: "Enable Notifications",
+    disabled: false,
+    value: false,
+  },
+  render: (args) => {
+    const [checkedSm, setCheckedSm] = useState(args.value ?? false);
+    const [checkedMd, setCheckedMd] = useState(args.value ?? false);
+    return (
+      <div className="flex flex-col gap-2 w-[266px]">
+        <Switch {...args} size="sm" value={checkedSm} onChange={setCheckedSm} />
+        <Switch {...args} size="md" value={checkedMd} onChange={setCheckedMd} />
+      </div>
+    );
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    size: "sm",
+    label: "Enable Notifications",
+    disabled: true,
+    value: false,
+  },
+  render: (args) => {
+    const [checked1, setChecked1] = useState(args.value ?? false);
+    const [checked2, setChecked2] = useState(args.value ?? false);
+    return (
+      <div className="flex flex-col gap-2 w-[266px]">
+        <Switch {...args} value={checked1} onChange={setChecked1} />
+        <Switch
+          {...args}
+          value={checked2}
+          onChange={setChecked2}
+          description="This is a description."
+        />
+      </div>
+    );
+  },
+};
+
+export const Classes: Story = {
+  args: {
+    size: "sm",
+    label: "Enable Notifications",
+    description: "This switch has a normal font.",
+    labelClassName: "font-normal",
+    disabled: false,
+    value: false,
   },
   render: (args) => {
     const [checked, setChecked] = useState(args.value ?? false);
-    return <Switch {...args} value={checked} onChange={setChecked} />;
+    return (
+      <div className="w-[266px]">
+        <Switch {...args} value={checked} onChange={setChecked} />
+      </div>
+    );
   },
-  argTypes: Label.argTypes,
 };
