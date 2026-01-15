bd59beb2e17125eb4f1b5bf59ec3ff346379384f
ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>
2026-01-15 21:55:55 +0530

    fix: eslint config file
    Signed-off-by: ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>

--- eslint.config.js
+++ eslint.config.js
@@ -5,27 +5,25 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
-import { globalIgnores } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 
-export default tseslint.config(
-  [
-    globalIgnores(["dist"]),
-    {
-      files: ["**/*.{ts,tsx}"],
-      extends: [
-        js.configs.recommended,
-        tseslint.configs.recommended,
-        reactHooks.configs["recommended-latest"],
-        reactRefresh.configs.vite,
-      ],
-      languageOptions: {
-        ecmaVersion: 2020,
-        globals: globals.browser,
-      },
-      rules: {
-        "@typescript-eslint/no-explicit-any": "warn",
-      },
+export default defineConfig([
+globalIgnores(["dist","storybook-static"]),
+  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.recommended,
+      reactRefresh.configs.vite,
+      storybook.configs["flat/recommended"],
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
     },
-  ],
-  storybook.configs["flat/recommended"]
-);
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
+]);
