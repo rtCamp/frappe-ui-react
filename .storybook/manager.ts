be9fa61069df0dc80a340af5a7a13d5d6ef98cbb
sayedtaqui <sayedwp@gmail.com>
2026-01-21 17:04:37 +0400

    Bump version

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,11 +1,11 @@
-import { addons } from "storybook/manager-api";
-import { create } from "storybook/theming";
+import { addons } from 'storybook/manager-api';
+import { create } from 'storybook/theming';
 
 addons.setConfig({
   theme: create({
-    base: "light",
+    base: 'light',
     brandTitle:
-      '<h1 id="fur-title">frappe-ui-react</h1> <span id="fur-version">(v1.0.3)</span>', // update version as per package.json
+      '<h1 id="fur-title">frappe-ui-react</h1> <span id="fur-version">(v1.0.4)</span>', // update version as per package.json
     brandUrl: undefined, // disables link on the title
     brandImage: undefined,
 
