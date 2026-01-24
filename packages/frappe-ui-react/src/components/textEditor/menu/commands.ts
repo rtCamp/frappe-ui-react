50e356addbbd53d606665a711daa4c56cee628eb
ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>
2026-01-24 22:36:57 +0530

    feat: add basic text editing features in text editor
    Signed-off-by: ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>

---
+++ packages/frappe-ui-react/src/components/textEditor/menu/commands.ts
@@ -0,0 +1,152 @@
+/**
+ * External dependencies.
+ */
+import {
+  AlignCenterIcon,
+  AlignLeftIcon,
+  AlignRightIcon,
+  BoldIcon,
+  Heading1Icon,
+  Heading2Icon,
+  Heading3Icon,
+  Heading4Icon,
+  Heading5Icon,
+  Heading6Icon,
+  ItalicIcon,
+  ListCheckIcon,
+  ListIcon,
+  ListOrderedIcon,
+  TextIcon,
+} from "lucide-react";
+
+/**
+ * Internal dependencies.
+ */
+import type { EditorCommand } from "../types";
+
+export const COMMANDS_KEYS = [
+  "paragraph",
+  "heading_1",
+  "heading_2",
+  "heading_3",
+  "heading_4",
+  "heading_5",
+  "heading_6",
+  "bold",
+  "italic",
+  "bullet_list",
+  "numbered_list",
+  "task_list",
+  "align_left",
+  "align_center",
+  "align_right",
+] as const;
+
+export type TYPE_COMMANDS_KEYS = (typeof COMMANDS_KEYS)[number];
+
+export const COMMANDS: Record<TYPE_COMMANDS_KEYS, EditorCommand> = {
+  paragraph: {
+    label: "Paragraph",
+    icon: TextIcon,
+    action: (editor) => editor.chain().focus().setParagraph().run(),
+    isActive: (editor) => editor.isActive("paragraph"),
+  },
+  heading_1: {
+    label: "Heading 1",
+    text: "H1",
+    icon: Heading1Icon,
+    action: (editor) =>
+      editor.chain().focus().toggleHeading({ level: 1 }).run(),
+    isActive: (editor) => editor.isActive("heading", { level: 1 }),
+  },
+  heading_2: {
+    label: "Heading 2",
+    text: "H2",
+    icon: Heading2Icon,
+    action: (editor) =>
+      editor.chain().focus().toggleHeading({ level: 2 }).run(),
+    isActive: (editor) => editor.isActive("heading", { level: 2 }),
+  },
+  heading_3: {
+    label: "Heading 3",
+    text: "H3",
+    icon: Heading3Icon,
+    action: (editor) =>
+      editor.chain().focus().toggleHeading({ level: 3 }).run(),
+    isActive: (editor) => editor.isActive("heading", { level: 3 }),
+  },
+  heading_4: {
+    label: "Heading 4",
+    text: "H4",
+    icon: Heading4Icon,
+    action: (editor) =>
+      editor.chain().focus().toggleHeading({ level: 4 }).run(),
+    isActive: (editor) => editor.isActive("heading", { level: 4 }),
+  },
+  heading_5: {
+    label: "Heading 5",
+    text: "H5",
+    icon: Heading5Icon,
+    action: (editor) =>
+      editor.chain().focus().toggleHeading({ level: 5 }).run(),
+    isActive: (editor) => editor.isActive("heading", { level: 5 }),
+  },
+  heading_6: {
+    label: "Heading 6",
+    text: "H6",
+    icon: Heading6Icon,
+    action: (editor) =>
+      editor.chain().focus().toggleHeading({ level: 6 }).run(),
+    isActive: (editor) => editor.isActive("heading", { level: 6 }),
+  },
+  bold: {
+    label: "Bold",
+    icon: BoldIcon,
+    action: (editor) => editor.chain().focus().toggleBold().run(),
+    isActive: (editor) => editor.isActive("bold"),
+  },
+  italic: {
+    label: "Italic",
+    icon: ItalicIcon,
+    action: (editor) => editor.chain().focus().toggleItalic().run(),
+    isActive: (editor) => editor.isActive("italic"),
+  },
+  bullet_list: {
+    label: "Bullet List",
+    icon: ListIcon,
+    action: (editor) => editor.chain().focus().toggleBulletList().run(),
+    isActive: (editor) => editor.isActive("bulletList"),
+  },
+  numbered_list: {
+    label: "Numbered List",
+    icon: ListOrderedIcon,
+    action: (editor) => editor.chain().focus().toggleOrderedList().run(),
+    isActive: (editor) => editor.isActive("orderedList"),
+  },
+  task_list: {
+    label: "Task List",
+    icon: ListCheckIcon,
+    action: (editor) => editor.chain().focus().toggleTaskList().run(),
+    isActive: (editor) => editor.isActive("taskList"),
+  },
+  align_center: {
+    label: "Align Center",
+    icon: AlignCenterIcon,
+    action: (editor) => editor.chain().focus().setTextAlign("center").run(),
+    isActive: (editor) => editor.isActive({ textAlign: "center" }),
+  },
+  align_left: {
+    label: "Align Left",
+    icon: AlignLeftIcon,
+    action: (editor) => editor.chain().focus().setTextAlign("left").run(),
+    isActive: (editor) => editor.isActive({ textAlign: "left" }),
+  },
+  align_right: {
+    label: "Align Right",
+    icon: AlignRightIcon,
+    action: (editor) => editor.chain().focus().setTextAlign("right").run(),
+    isActive: (editor) => editor.isActive({ textAlign: "right" }),
+  },
+};
+
+export default COMMANDS;
---
+++ packages/frappe-ui-react/src/components/textEditor/menu/menu.tsx
@@ -0,0 +1,153 @@
+/**
+ * External dependencies.
+ */
+
+import { useCurrentEditor, useEditorState } from "@tiptap/react";
+import clsx from "clsx";
+
+/**
+ * Internal dependencies.
+ */
+import COMMANDS, { type TYPE_COMMANDS_KEYS } from "./commands";
+import type { EditorCommand } from "../types";
+import { Popover } from "../../popover";
+
+export interface MenuProps {
+  className?: string;
+}
+
+const DEFAULT_COMMANDS: Array<
+  TYPE_COMMANDS_KEYS | "separator" | Array<TYPE_COMMANDS_KEYS>
+> = [
+  [
+    "heading_1",
+    "heading_2",
+    "heading_3",
+    "heading_4",
+    "heading_5",
+    "heading_6",
+  ],
+  "paragraph",
+  "separator",
+  "bold",
+  "italic",
+  "separator",
+  "bullet_list",
+  "numbered_list",
+  "task_list",
+  "separator",
+  "align_left",
+  "align_center",
+  "align_right",
+  "separator",
+];
+
+const Menu = ({ className }: MenuProps) => {
+  const { editor } = useCurrentEditor();
+  const editorState = useEditorState({
+    editor,
+    selector: (ctx) => ({
+      selection: ctx?.editor?.state?.selection,
+      focused: ctx?.editor?.state?.selection,
+    }),
+  });
+
+  const isButtonActive = (button: EditorCommand): boolean => {
+    if (editor && editorState && editorState.focused && button.isActive) {
+      return button.isActive(editor);
+    }
+    return false;
+  };
+
+  if (!editor) {
+    return null;
+  }
+
+  return (
+    <div
+      className={clsx(
+        "flex bg-surface-white px-1 py-1 w-full overflow-x-auto rounded-t-lg border border-outline-gray-modals items-center",
+        className
+      )}
+    >
+      {DEFAULT_COMMANDS.map((command_key, index) => {
+        if (Array.isArray(command_key)) {
+          const activeCommand = editorState?.focused
+            ? command_key.find((b) => COMMANDS[b].isActive(editor)) ||
+              command_key[0]
+            : command_key[0];
+
+          const ActiveIcon = COMMANDS[activeCommand].icon;
+
+          return (
+            <div key={index} className="shrink-0">
+              <Popover
+                transition="default"
+                target={({ togglePopover }) => (
+                  <button
+                    className="rounded px-2 py-1 text-base font-medium text-ink-gray-8 transition-colors hover:bg-surface-gray-2"
+                    onClick={() => togglePopover()}
+                  >
+                    <ActiveIcon className="h-4 w-4" />
+                  </button>
+                )}
+                body={({ close }) => (
+                  <ul className="w-fit p-1.5 mt-2 rounded-lg bg-surface-modal shadow-2xl ring-1 ring-black/5 focus:outline-none">
+                    {command_key.map((command_key, optionIndex) => {
+                      const command = COMMANDS[command_key];
+                      return (
+                        <li key={optionIndex}>
+                          <button
+                            className="w-full h-7 rounded px-2 text-base flex items-center gap-2 hover:bg-surface-gray-3"
+                            onClick={() => {
+                              command.action(editor);
+                              close();
+                            }}
+                            title={command.label}
+                          >
+                            <command.icon className="size-4 flex-shrink-0 text-ink-gray-6" />
+                            <span className="whitespace-nowrap text-ink-gray-7">
+                              {command.label}
+                            </span>
+                          </button>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                )}
+              />
+            </div>
+          );
+        }
+        if (command_key == "separator") {
+          return (
+            <div
+              key={index}
+              className="h-4 w-0.5 border-l border-outline-gray-1"
+            ></div>
+          );
+        }
+        const command: EditorCommand = COMMANDS[command_key];
+        const label = command.label;
+        const Icon = command.icon;
+        return (
+          <button
+            key={index}
+            className={clsx(
+              "flex rounded p-1 text-ink-gray-8 transition-colors",
+              isButtonActive(command)
+                ? "bg-surface-gray-3"
+                : "hover:bg-surface-gray-2"
+            )}
+            onClick={() => command.action(editor)}
+            title={label}
+          >
+            <Icon className="size-4" />
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Menu;
