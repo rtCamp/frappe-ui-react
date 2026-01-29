ea224abc3c734b8f0cb1b7d563e897d1c3189320
ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>
2026-01-29 16:54:18 +0530

    fix: add title to button and highlight color
    Signed-off-by: ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>

--- packages/frappe-ui-react/src/components/textEditor/menu/commands/fontColor.tsx
+++ packages/frappe-ui-react/src/components/textEditor/menu/commands/fontColor.tsx
@@ -8,7 +8,6 @@ import { useCurrentEditor } from "@tiptap/react";
  * Internal dependencies.
  */
 import { Popover } from "../../../popover";
-import { Tooltip } from "../../../tooltip";
 
 interface FontColorProps {
   children: (props: {
@@ -17,49 +16,54 @@ interface FontColorProps {
   }) => React.ReactNode;
 }
 
-const foregroundColors: string[] = [
-  "#171717",
-  "#cc2929",
-  "#d45a08",
-  "#d1930d",
-  "#0f814a",
-  "#13564f",
-  "#0b9e92",
-  "#007be0",
-  "#8642c2",
-  "#cf3a96",
-  "#343434",
+type Color = {
+  name: string;
+  value: string;
+};
+
+const foregroundColors: Color[] = [
+  { name: "Default", value: "#171717" },
+  { name: "Red", value: "#cc2929" },
+  { name: "Orange", value: "#d45a08" },
+  { name: "Yellow", value: "#d1930d" },
+  { name: "Green", value: "#278f5e" },
+  { name: "Teal", value: "#0b9e92" },
+  { name: "Cyan", value: "#32a4c7" },
+  { name: "Blue", value: "#007be0" },
+  { name: "Purple", value: "#8642c2" },
+  { name: "Pink", value: "#cf3a96" },
+  { name: "Gray", value: "#7c7c7c" },
 ];
 
-const backgroundColors: string[] = [
-  "#000000b8",
-  "#ffe7e7",
-  "#ffefe4",
-  "#fff7d3",
-  "#e4faeb",
-  "#e6f7f4",
-  "#ddf7ff",
-  "#e6f4ff",
-  "#f6e9ff",
-  "#fde8f5",
-  "#d4d4d4",
+const backgroundColors: Color[] = [
+  { name: "Default", value: "#ffffff" },
+  { name: "Red", value: "#ffe7e7" },
+  { name: "Orange", value: "#ffefe4" },
+  { name: "Yellow", value: "#fff7d3" },
+  { name: "Green", value: "#e4faeb" },
+  { name: "Teal", value: "#e6f7f4" },
+  { name: "Cyan", value: "#ddf7ff" },
+  { name: "Blue", value: "#e6f4ff" },
+  { name: "Purple", value: "#f6e9ff" },
+  { name: "Pink", value: "#fde8f5" },
+  { name: "Gray", value: "#f3f3f3" },
 ];
 
 const FontColor = ({ children }: FontColorProps) => {
   const { editor } = useCurrentEditor();
 
-  const setForegroundColor = (color: string) => {
+  const setForegroundColor = (color: Color) => {
     if (!editor) {
       return;
     }
-    editor.chain().focus().setColor(color).run();
+    editor.chain().focus().setColor(color.value).run();
   };
 
-  const setBackgroundColor = (color: string) => {
+  const setBackgroundColor = (color: Color) => {
     if (!editor) {
       return;
     }
-    editor.chain().focus().setHighlight({ color }).run();
+    editor.chain().focus().setHighlight({ color: color.value }).run();
   };
 
   return (
@@ -77,48 +81,45 @@ const FontColor = ({ children }: FontColorProps) => {
             <div className="text-sm text-ink-gray-7">Text Color</div>
             <div className="mt-1 grid grid-cols-6 gap-1">
               {foregroundColors.map((color) => (
-                <div key={color} className="flex">
-                  <Tooltip text={color}>
-                    <button
-                      aria-label={color}
-                      className={clsx(
-                        "flex shrink-0 h-5 w-5 items-center justify-center rounded border border-outline-gray-1 text-base"
-                      )}
-                      style={{
-                        color,
-                      }}
-                      onClick={() => {
-                        setForegroundColor(color);
-                        close();
-                      }}
-                    >
-                      A
-                    </button>
-                  </Tooltip>
-                </div>
+                <button
+                  key={color.name}
+                  aria-label={color.name}
+                  className={clsx(
+                    "flex shrink-0 h-5 w-5 items-center justify-center rounded border border-outline-gray-1 text-base"
+                  )}
+                  style={{
+                    color: color.value,
+                  }}
+                  onClick={() => {
+                    setForegroundColor(color);
+                    close();
+                  }}
+                  title={color.name}
+                >
+                  A
+                </button>
               ))}
             </div>
             <div className="text-sm text-ink-gray-7">Background Color</div>
             <div className="mt-1 grid grid-cols-6 gap-1">
               {backgroundColors.map((color) => (
-                <div key={color} className="flex">
-                  <Tooltip text={color}>
-                    <button
-                      aria-label={color}
-                      className={clsx(
-                        "flex shrink-0 h-5 w-5 items-center justify-center rounded border border-outline-gray-1 text-base"
-                      )}
-                      style={{
-                        backgroundColor: color,
-                      }}
-                      onClick={() => {
-                        setBackgroundColor(color);
-                        close();
-                      }}
-                    >
-                      A
-                    </button>
-                  </Tooltip>
+                <div key={color.name} className="flex">
+                  <button
+                    title={color.name}
+                    aria-label={color.name}
+                    className={clsx(
+                      "flex shrink-0 h-5 w-5 items-center justify-center rounded border border-outline-gray-1 text-base"
+                    )}
+                    style={{
+                      backgroundColor: color.value,
+                    }}
+                    onClick={() => {
+                      setBackgroundColor(color);
+                      close();
+                    }}
+                  >
+                    A
+                  </button>
                 </div>
               ))}
             </div>
