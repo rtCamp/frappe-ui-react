b19efb3aef1ba4fb759bf97ca58ccf15cb897c78
Aditya Dhade <aditya.dhade@rtcamp.com>
2026-02-05 13:20:26 +0530

    Add ButtonGroup component

---
+++ packages/frappe-ui-react/src/components/buttonGroup/types.ts
@@ -0,0 +1,14 @@
+import type {
+  ButtonProps,
+  ButtonSize,
+  ButtonTheme,
+  ButtonVariant,
+} from "../button/types";
+
+export interface ButtonGroupProps {
+  buttons: (ButtonProps & { id?: string })[];
+  className?: string;
+  size?: ButtonSize;
+  variant?: ButtonVariant;
+  theme?: ButtonTheme;
+}
