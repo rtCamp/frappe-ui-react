f3640e895e1fddbb2f4b3a1c6979e8c66e7b4c44
Aditya Dhade <aditya.dhade@rtcamp.com>
2026-02-05 15:58:07 +0530

    Fix incorrect preview size for dragged widget from widget gallery

--- packages/frappe-ui-react/src/components/dashboardGrid/index.ts
+++ packages/frappe-ui-react/src/components/dashboardGrid/index.ts
@@ -1,4 +1,5 @@
 export { DashboardGrid, default } from './dashboard';
 export { WidgetWrapper } from './widgetWrapper';
 export { DashboardWidgetGallery } from './dashboardWidgetGallery';
+export { DashboardProvider, useDashboardContext } from './dashboardContext';
 export * from "./types";
