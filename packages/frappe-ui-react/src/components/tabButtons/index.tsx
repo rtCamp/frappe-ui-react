6fc4f7df7ae7679c4aeb5db5e2f34a1c42316b1a
ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>
2026-03-03 12:45:00 +0530

    chore: refactor tabbuttons to use toggle group from base ui
    Signed-off-by: ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>

--- packages/frappe-ui-react/src/components/tabButtons/index.tsx
+++ packages/frappe-ui-react/src/components/tabButtons/index.tsx
@@ -1,11 +1,17 @@
-import { Radio, RadioGroup } from "@headlessui/react";
+/**
+ * External dependencies.
+ */
+import { Toggle } from "@base-ui/react/toggle";
+import { ToggleGroup } from "@base-ui/react/toggle-group";
 
-import { Button } from "../button";
+/**
+ * Internal dependencies.
+ */
 import { cn } from "../../utils";
 
 interface TabButtonItem {
   label: string;
-  value: string | number;
+  value: string;
   disabled?: boolean;
   hideLabel?: boolean;
   onClick?: () => void;
@@ -15,8 +21,8 @@ interface TabButtonItem {
 
 interface TabButtonsProps {
   buttons: TabButtonItem[];
-  value: string | number;
-  onChange: (value: string | number) => void;
+  value: string;
+  onChange: (value: string) => void;
   className?: string;
 }
 
@@ -27,55 +33,33 @@ const TabButtons = ({
   className = "",
 }: TabButtonsProps) => {
   return (
-    <RadioGroup
-      value={value}
-      onChange={onChange}
+    <ToggleGroup
+      value={[value]}
+      onValueChange={(val) => {
+        if (val[0]) {
+          onChange(val[0]);
+        }
+      }}
       className={cn(
-        "flex space-x-0.5 rounded-md bg-surface-gray-2 h-7 items-center px-[1px] text-sm border border-gray-200",
+        "flex space-x-0.5 rounded-md bg-surface-gray-2 h-7 items-center p-px text-sm border border-outline-gray-2",
         className
       )}
     >
-      {buttons.map((button) => (
-        <Radio
-          key={button.value || button.label}
-          value={button.value || button.label}
-          disabled={button.disabled}
-          className="focus:outline-none group flex-1"
-        >
-          {({ checked, focus }) => (
-            <Button
-              onClick={button.onClick}
-              disabled={button.disabled}
-              {...Object.fromEntries(
-                Object.entries(button).filter(
-                  ([key]) =>
-                    ![
-                      "value",
-                      "disabled",
-                      "label",
-                      "hideLabel",
-                      "onClick",
-                    ].includes(key)
-                )
-              )}
-              className={`!h-6.5 w-full${
-                focus ? "ring-outline-gray-2 focus-visible:ring" : ""
-              } ${checked ? "!bg-surface-white" : ""} ${
-                button.disabled
-                  ? ""
-                  : checked
-                    ? "text-ink-gray-9 shadow"
-                    : "!text-ink-gray-5"
-              } cursor-pointer`}
-            >
-              {!button.hideLabel && button.label && (
-                <span className="flex h-4 items-center">{button.label}</span>
-              )}
-            </Button>
-          )}
-        </Radio>
-      ))}
-    </RadioGroup>
+      {buttons.map((button) => {
+        return (
+          <Toggle
+            className={cn(
+              "rounded-md px-2 outline-black group flex-1 h-6.5 w-full data-pressed:bg-surface-white text-nowrap disabled:text-ink-gray-5"
+            )}
+            aria-label={button.label}
+            value={button.value}
+            disabled={button.disabled}
+          >
+            <p className="flex h-4 items-center">{button.label}</p>
+          </Toggle>
+        );
+      })}
+    </ToggleGroup>
   );
 };
 
