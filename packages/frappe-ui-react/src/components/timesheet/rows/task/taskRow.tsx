e0f7e525f63492d188007f5652cfda574e7b7f9c
Aditya Dhade <aditya.dhade@rtcamp.com>
2026-03-06 00:19:14 +0530

    Add TaskRow component for Timesheet

---
+++ packages/frappe-ui-react/src/components/timesheet/rows/task/taskRow.tsx
@@ -0,0 +1,117 @@
+/**
+ * External dependencies.
+ */
+import { Plus, Star } from "lucide-react";
+
+/**
+ * Internal dependencies.
+ */
+import { Button } from "../../../button";
+import { cn } from "../../../../utils";
+import { statusIcon, statusIconVariants, type MemberStatus } from "./constants";
+import { BASE_PADDING, NESTING_OFFSET } from "../constants";
+
+export interface TaskRowProps {
+  /** Optional index of the task, used for identifying the task in callbacks. */
+  taskIndex?: number;
+  /** Label for the task row. */
+  label?: string;
+  /** Nesting level for the task row, used for indentation. */
+  nesting?: number;
+  /** Whether the task row is starred. */
+  starred?: boolean;
+  /** Array of time entries for each day of the week for the task. */
+  timeEntries: { time: string; nonBillable?: boolean; disabled?: boolean }[];
+  /** Optional function to handle cell click events, receiving the task index and day index. */
+  onCellClick?: (taskIndex: number | undefined, dayIndex: number) => void;
+  /** Total hours logged for the week. */
+  totalHours?: string;
+  /** Status of the timesheet for the task row. */
+  status?: MemberStatus;
+  /** Additional class names for the task row container. */
+  className?: string;
+}
+
+export const TaskRow: React.FC<TaskRowProps> = ({
+  taskIndex,
+  label,
+  nesting = 0,
+  starred = false,
+  timeEntries,
+  onCellClick,
+  totalHours = "",
+  status = "open",
+  className,
+}) => {
+  const StatusIcon = statusIcon[status];
+  return (
+    <div
+      className={cn(
+        "flex items-center border-b border-outline-gray-1 transition-colors w-full justify-between px-1 py-2",
+        className
+      )}
+      style={{ paddingLeft: `${BASE_PADDING + nesting * NESTING_OFFSET}px` }}
+    >
+      <div className="min-w-0 flex flex-1 items-center">
+        <div className="min-w-0 flex items-center gap-2">
+          <span className="shrink-0">
+            <StatusIcon
+              strokeWidth={1.5}
+              size={16}
+              className={statusIconVariants({ status })}
+            />
+          </span>
+          <span className="text-base font-medium truncate min-w-0">
+            {label}
+          </span>
+          {starred ? (
+            <span className="shrink-0">
+              <Star
+                strokeWidth={1.5}
+                size={16}
+                className="fill-current text-ink-amber-2"
+              />
+            </span>
+          ) : null}
+        </div>
+      </div>
+      {timeEntries.map((timeEntry, index) => {
+        return (
+          <div
+            key={index}
+            className="shrink-0 flex justify-end items-center text-base text-ink-gray-6 whitespace-nowrap w-16 h-7 px-2 py-1.5 leading-3.5"
+          >
+            <Button
+              variant="ghost"
+              className="w-14.25 relative group flex justify-center items-center !disabled:hover:bg-surface-gray-2 !disabled:focus:bg-surface-gray-2 !disabled:active:bg-surface-gray-3 disabled:cursor-default!"
+              disabled={timeEntry.disabled}
+              onClick={() => onCellClick?.(taskIndex, index)}
+            >
+              {timeEntry.time === "" ? (
+                <>
+                  <span className="group-hover:hidden flex-1 ml-1.5 text-center text-ink-gray-4">
+                    -
+                  </span>
+                  <span className="group-hover:flex hidden absolute w-full h-full top-0 left-0 justify-center items-center">
+                    <Plus strokeWidth={1.5} size={16} className="" />
+                  </span>
+                </>
+              ) : (
+                <span>{timeEntry.time}</span>
+              )}
+              {timeEntry.nonBillable ? (
+                <span className="block absolute z-10 -bottom-0.5 left-1/2 w-1 h-1 rounded-full bg-surface-amber-3 transform -translate-x-1/2"></span>
+              ) : null}
+            </Button>
+          </div>
+        );
+      })}
+
+      <div className="shrink-0 flex justify-end items-center text-base text-end text-ink-gray-6 whitespace-nowrap w-16 h-7 px-2 py-1.5">
+        <span>{totalHours}</span>
+      </div>
+
+      <div className="shrink-0 w-12 h-7"></div>
+    </div>
+  );
+};
