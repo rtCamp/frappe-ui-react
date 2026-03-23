f99cd0d849abfcdbef22d678780b83ea793fcc1b
Aditya Dhade <aditya.dhade@rtcamp.com>
2026-03-23 13:15:45 +0530

    feat: add overdue icon

--- packages/frappe-ui-react/src/icons/index.tsx
+++ packages/frappe-ui-react/src/icons/index.tsx
@@ -9,3 +9,4 @@ export { default as Reports } from "./reports.svg?react";
 export { default as Search } from "./search.svg?react";
 export { default as Tasks } from "./tasks.svg?react";
 export { default as Time } from "./time.svg?react";
+export { default as Overdue } from "./overdue.svg?react";
