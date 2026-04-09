9f7002aacd5d6ebffbb1a02fb55b64a4551712b2
ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>
2026-04-09 20:42:40 +0530

    fix: getNumDays
    Signed-off-by: ayushnirwal <53055971+ayushnirwal@users.noreply.github.com>

--- packages/frappe-ui-react/src/components/gantt-view/gantt-bar/utilities/getNumDays.ts
+++ packages/frappe-ui-react/src/components/gantt-view/gantt-bar/utilities/getNumDays.ts
@@ -11,8 +11,13 @@ export function getNumDays(
   const calDays = differenceInCalendarDays(start, end);
 
   const startDay = (end.getDay() + 6) % 7;
+  const r = calDays % 7;
+  const satOffset = (5 - startDay + 7) % 7;
+  const sunOffset = (6 - startDay + 7) % 7;
   const numWeekendDays =
-    Math.floor(calDays / 7) * 2 + Math.max(0, (calDays % 7) + startDay - 5);
+    Math.floor(calDays / 7) * 2 +
+    (satOffset < r ? 1 : 0) +
+    (sunOffset < r ? 1 : 0);
 
   return calDays - (showWeekend ? 0 : numWeekendDays);
 }
