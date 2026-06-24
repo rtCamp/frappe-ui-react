9c8ecddde5ed5a3c7665ad37f00fed354c7c7287
Praveen Kumar <praveenkum1102@gmail.com>
2026-06-24 23:00:56 +0530

    export new icons

--- packages/frappe-ui-react/src/icons/line/index.tsx
+++ packages/frappe-ui-react/src/icons/line/index.tsx
@@ -456,3 +456,5 @@ export { default as XAxis } from "./x-axis.svg?react";
 export { default as YAxis } from "./y-axis.svg?react";
 export { default as Youtube } from "./youtube.svg?react";
 export { default as Zap } from "./zap.svg?react";
+export { default as TimeOff } from "./time-off.svg?react";
+export { default as Clock } from "./clock.svg?react";
