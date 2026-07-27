d356f53e8c956e93a2fd7575bd65dde76bb9bae7
Ayush Nirwal <coding.carrots@gmail.com>
2026-07-27 17:39:26 +0530

    feat: add renderDropdown prop to breadCrumbs
    Signed-off-by: Ayush Nirwal <coding.carrots@gmail.com>

--- packages/frappe-ui-react/src/components/breadcrumbs/types.ts
+++ packages/frappe-ui-react/src/components/breadcrumbs/types.ts
@@ -1,4 +1,5 @@
 import type { ReactNode } from "react";
+import type { useRender } from "@base-ui/react/use-render";
 import type { DropdownProps } from "../dropdown";
 
 export interface BreadcrumbItem {
@@ -22,4 +23,14 @@ export interface BreadcrumbsProps {
   separatorClassName?: string;
   renderPrefix?: (item: BreadcrumbItem) => ReactNode;
   renderSuffix?: (item: BreadcrumbItem) => ReactNode;
+  /**
+   * Replace the default `<Dropdown>` wrapper of a crumb that has `dropdown`
+   * set. Accepts a React element or a render function that receives the
+   * merged props (`children` is the crumb trigger) and the crumb's `state`.
+   */
+  renderDropdown?: useRender.RenderProp<BreadcrumbDropdownState>;
 }
+
+export type BreadcrumbDropdownState = {
+  item: BreadcrumbItem;
+};
